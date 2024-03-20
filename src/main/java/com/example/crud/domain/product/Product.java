4f34de21d254a44a8267d8329c3d54c00de07f0e
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-20 15:49:00 -0300

    Creating Table using Flyway and Entity Product

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,21 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    private Number price_in_cents;
+
+}
