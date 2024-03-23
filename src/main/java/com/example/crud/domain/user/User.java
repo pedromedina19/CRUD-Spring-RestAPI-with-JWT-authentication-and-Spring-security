4bd16bf4c84d2b789660cba201aabb72bb7e9b98
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-23 13:11:18 -0300

    Update User.java

--- src/main/java/com/example/crud/domain/user/User.java
+++ src/main/java/com/example/crud/domain/user/User.java
@@ -1,7 +1,6 @@
 package com.example.crud.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,6 +19,8 @@ import java.util.List;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String login;
     private String password;
